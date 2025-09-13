e31df36b1115cf70e043e5cc5283684de87d276b
Chad Burrow <stevenchadburrow@gmail.com>
2025-09-13 08:13:31 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ sudo echo "exit 0" >> /etc/rc.local
 sudo chmod +rwx /etc/rc.local
 sudo chown root /etc/rc.local
 
-gcc -o switch.o switch.c
-gcc -o buttons.o buttons.c
-gcc -o menu.o menu.c
-gcc -O3 -o PeanutGB.o main.c
+gcc -o /home/username/PeanutGB/switch.o /home/username/PeanutGB/switch.c
+gcc -o /home/username/PeanutGB/buttons.o /home/username/PeanutGB/buttons.c
+gcc -o /home/username/PeanutGB/menu.o /home/username/PeanutGB/menu.c
+gcc -O3 -o /home/username/PeanutGB/PeanutGB.o /home/username/PeanutGB/main.c
 
 echo "Installed PeanutGB!"
 echo "If this is a fresh install, type:"
